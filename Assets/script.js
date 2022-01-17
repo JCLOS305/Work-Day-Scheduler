5bed6c20bf8cfd96d24bc931573234472245f6a0
jclos305 <jclos305@hotmail.com>
2022-01-16 21:26:42 -0500

    save to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -89,11 +89,16 @@ var interval = setInterval(function() {
     initPage()
     background()
   
-    // Buttons (save to Local Storage)
-    $(".saveBtn").on("click", function(){
-      userInput = $(this).siblings(".form-control").val().trim();
-      console.log(userInput);
-      hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-      console.log(hourSpan);
-      localStorage.setItem(hourSpan, JSON.stringify(userInput));
-    });
+   
+  // Buttons (save to Local Storage)
+  $(".saveBtn").on("click", function(){
+    userInput = $(this).siblings(".form-control").val().trim();
+    console.log(userInput);
+    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+    console.log(hourSpan);
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+
+  })
+
+
+});
